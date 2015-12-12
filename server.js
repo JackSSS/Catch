da0fc2a4bf82a7a52471107f88c16ec3c348876b
Jack Sneed <sneed.jack@gmail.com>
2015-12-12 14:51:40 -0800

    switch to side-menu from blank

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/catch_dev');
 
-var catchRouter = require(__dirname + '/routes/catch_routes');
+//var catchRouter = require(__dirname + '/routes/catch_routes');
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/api', catchRouter);
+//app.use('/api', catchRouter);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {

cc47fe8a0b1291a696c4e1f4da3ba35028d3c851
adam-james <adam.j.sulewski@gmail.com>
2015-12-14 11:36:34 -0800

    add spinner to login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('catch.controllers', [])
 
-.controller('AppCtrl', function($scope, $timeout, $location) {
+.controller('AppCtrl', function($scope, $timeout, $location, $ionicLoading) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -16,10 +16,13 @@ angular.module('catch.controllers', [])
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
-
+    $ionicLoading.show({
+      template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Signing in...'
+    });
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
     $timeout(function() {
+      $ionicLoading.hide();
       $location.path('/home/panic');
     }, 1000);
   };

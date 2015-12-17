e41c93d0d58383e16d54cda174b2cd9b73230e47
Jack Sneed <sneed.jack@gmail.com>
2015-12-16 16:03:14 -0800

    bundle

--- www/js/bundle.js
+++ www/js/bundle.js
@@ -272,19 +272,50 @@
 /***/ function(module, exports) {
 
 	module.exports = function(app) {
-	  app.controller('SearchController', ['$scope', function($scope) {
+	  app.controller('SearchController', ['$scope', '$http', function($scope, $http) {
 	    var data = [{ username: "jack"}, {username: "motley"}]
 	    var search = [{ username: "jack"}, {username: "motley"}]
-	    $scope.users = [];
+	    $scope.contacts = [];
 
-	   $scope.getAll = function() {
-	      $scope.users = data;
+	    $scope.getAll = function() {
+	     $http.get('/api/contacts/:userId')
+	        .then(function(res){
+	          $scope.contacts = res.data
+	          }, function(err) {
+	            console.log(err.data)
+	          });
 	    };
 
-	    $scope.search = function() {
-	      var query = {username: new RegExp(usernameText, 'i')};
-	      username.find(query);
+	    $scope.search = function(contact) {
+	      $http.post('/api/contacts/search', contact)
+	        .then(function(res) {
+	          $scope.contacts.search(res.data);
+	        }, function(err) {
+	          console.log(err.data)
+	        });
+
 	    };
+
+	    $scope.add = function(contact) {
+	      $http.post('/api/contacts/add', contact)
+	        .then(function(res) {
+	          $scope.contacts.send(userId, contactId);
+	        }, function(err) {
+	          console.log(err.data)
+	        });
+
+	    };
+
+	    $scope.confirm = function(contact) {
+	      $http.post('/api/contacts/request', contact)
+	        .then(function(res) {
+	          $scope.contacts.confirm(contactId, requesterId);
+	        }, function(err) {
+	          console.log(err.data)
+	        });
+
+	    };
+
 	  }]);
 	};
 

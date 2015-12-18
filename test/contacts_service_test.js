6f1c58a422ad576c232a2415e5be3bf9530f80e8
adam-james <adam.j.sulewski@gmail.com>
2015-12-18 00:38:41 -0800

    start service tests

---
+++ test/contacts_service_test.js
@@ -0,0 +1,39 @@
+require(__dirname + '/../www/js/app');
+require('angular-mocks');
+
+describe('Contacts Service', function() {
+  var $httpBackend;
+  var Contacts;
+
+  beforeEach(angular.mock.module('catch'));
+
+  beforeEach(angular.mock.inject(function(_Contacts_) {
+    Contacts = _Contacts_;
+  }));
+
+  it('should inject service', function() {
+    expect(typeof Contacts).toBe('object');
+  });
+
+  describe('REST requests', function() {
+    beforeEach(angular.mock.inject(function(_$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+    }));
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should get all contacts', function() {
+      var userId = '123';
+      $httpBackend.expectGET('http://localhost:3000/api/contacts/123')
+        .respond(200, []);
+      Contacts.getAll(userId, function(err, data) {
+        expect(err).toBe(null);
+        expect(Array.isArray(data)).toBe(true);
+      });
+      $httpBackend.flush();
+    });
+  });
+});

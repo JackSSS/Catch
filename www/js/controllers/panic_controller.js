8a595f0e6c54058194128c8505a94d2c9ead2b85
Jack Sneed <sneed.jack@gmail.com>
2015-12-19 23:28:20 -0800

    Initial geo watcher for alert contacts

--- www/js/controllers/panic_controller.js
+++ www/js/controllers/panic_controller.js
@@ -1,6 +1,8 @@
 module.exports = function(app) {
 
   app.controller('PanicCtrl', ['$scope', '$ionicPopup', function($scope, $ionicPopup) {
+    var lat = $rootScope.lat;
+    var lng = $rootScope.lng;
 
       $scope.showAlert = function() {
         $ionicPopup.alert({
@@ -9,5 +11,35 @@ module.exports = function(app) {
           okType: 'button-dark'
         });
       };
-    }]);
+
+      $scope.displayAndWatch = function(position) {
+        setCurrentPosition(position);
+        watchCurrentPosition();
+      }
+
+      $scope.setCurrentPosition = function(pos) {
+        currentPositionMarker = new google.maps.Marker({
+            map: map,
+            position: new google.maps.LatLng(lat, lng),
+            title: "Current Position"
+        });
+        map.panTo(new google.maps.LatLng(lat, lng));
+      }
+
+      $scope.watchCurrentPosition = function() {
+        var positionTimer = navigator.geolocation.watchPosition(
+          function (position) {
+            setMarkerPosition(
+              currentPositionMarker,
+              position
+            );
+          });
+      }
+
+      $scope.setMarkerPosition = function(marker, position) {
+        marker.setPosition(
+          new google.maps.LatLng(lat, lng)
+        );
+      }
+  }]);
 };

bf1633b14dac0b4447d806eb52d6fea63b131b5d
adam-james <adam.j.sulewski@gmail.com>
2015-12-20 16:07:12 -0800

    make sendPush function and make push work again

--- routes/contacts_routes.js
+++ routes/contacts_routes.js
@@ -3,8 +3,7 @@ var mongoose = require('mongoose');
 var jsonParser = require('body-parser').json();
 var User = require(__dirname + '/../models/user');
 var handleError = require(__dirname + '/../lib/handle_server_error');
-var request = require('superagent');
-var config = require(__dirname + '/../config');
+var sendPush = require(__dirname + '/../lib/send_push');
 var decryptUser = require(__dirname + '/../lib/decrypt_user');
 
 var contactsRouter = module.exports = exports = express.Router();
@@ -105,33 +104,24 @@ contactsRouter.post('/contacts/search', decryptUser, jsonParser, function(req, r
   });
 });
 
-contactsRouter.post('/contacts/alert', decryptUser, jsonParser, function(req, res) {
+contactsRouter.post('/contacts/alert', jsonParser, function(req, res) {
   var user = req.body.user;
   var contactsIds = user.contacts;
 
   User.find({_id: {$in: contactsIds}}, function(err, contacts) {
     if (err) return handleError(err, res);
 
+    debugger;
     var deviceIds = contacts.map(function(contact) {
       return contact.deviceId;
     });
 
-    request
-      .post('https://push.ionic.io/api/v1/push')
-      .set('Content-Type', 'application/json')
-      .set('X-Ionic-Application-Id', config.ionicAppId)
-      .auth(config.ionicApiKey)
-      .send({
-        tokens: deviceIds,
-        notification: {
-          alert: user.username + ' pushed the alert button!'
-        }
-      })
-      .end(function(error, response) {
-        if (error) return handleError(error, res);
+    var message = user.username + ' pushed the alert button!';
 
-        console.log(response.body);
-        res.json(response.body);
-      });
+    sendPush(deviceIds, message, function(error, response) {
+      if (error) return handleError(error, res);
+
+      res.json(response);
+    });
   });
 });
